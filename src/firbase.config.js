c7ce4db44498fad0b8d71cba9914208536bce106
jang walia <jangiwalia83@gmail.com>
2022-04-20 10:00:51 -0700

    initial commit

---
+++ src/firbase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgbkd7wLr1MHa2WBhFyeo3Pl2hVz8NP1s",
+  authDomain: "click-list-8ac18.firebaseapp.com",
+  projectId: "click-list-8ac18",
+  storageBucket: "click-list-8ac18.appspot.com",
+  messagingSenderId: "445414004157",
+  appId: "1:445414004157:web:44dd873cf2fea48910aed4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
