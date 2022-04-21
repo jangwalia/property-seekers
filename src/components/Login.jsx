d037300b7d7a452e6e34a26234ea57e4b01a68ca
jang walia <jangiwalia83@gmail.com>
2022-04-21 09:28:23 -0700

    create login and sign up form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 export default function Login() {
+  const [showPassword,setShowPassword] = useState(false)
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const navigate = useNavigate()
+  // handling email input
+  const emailChange = (e) =>{
+    setEmail(e.target.value)
+  }
+  //handling password input
+  const passwordChange = (e) =>{
+    setPassword(e.target.value)  
+  }
+  //handle showpassword
+  const checkShowPassword = ()=>{
+    setShowPassword((prevState) => !prevState)
+  }
   return (
     <div>
-      <h1>Log In Page</h1>
+      <div className="pageContainer">
+        <header>
+           <p className="pageHeadre">
+             Welcome Back!
+           </p>
+        </header>
+        <main>
+          <form>
+            <input onChange = {emailChange} type="text" className='emailInput' placeholder='Email' id = "email" value = {email} />
+            <div className='passwordInputDiv'>
+              <input type= {showPassword ? "text" : "password"} onChange = {passwordChange} className = 'passwordInput' placeholder='Password' value = {password} id = 'password' />
+              <img src= {visibilityIcon} alt="showPassword" className='showPassword' onClick = {() => checkShowPassword()} />
+            </div>
+          <Link to= '/forgotPassword' className='forgotPasswordLink' >forgot password</Link>
+            <div className='signInBar'>
+              <p className="signInText">
+                Log In
+              </p>
+              <button className='signInButton'>
+                <ArrowRightIcon fill = 'white' width = '34px' height = '34px'/>
+              </button>
+            </div>
+          </form>
+          <Link to='/signup' className='registerLink'>Register Instead</Link>
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
 export default function Signin() {
+  const [showPassword,setShowPassword] = useState(false)
+  const [email,setEmail] = useState('')
+  const [name,setName] = useState('')
+  const [password,setPassword] = useState('')
+  const navigate = useNavigate()
+  // handling email input
+  const emailChange = (e) =>{
+    setEmail(e.target.value)
+  }
+  //handling nameChange
+  const nameChange = (e) =>{
+    setName(e.target.vale)
+  }
+  //handling password input
+  const passwordChange = (e) =>{
+    setPassword(e.target.value)  
+  }
+  //handle showpassword
+  const checkShowPassword = ()=>{
+    setShowPassword((prevState) => !prevState)
+  }
   return (
     <div>
-      <h1>Sign Up Page</h1>
+      <div className="pageContainer">
+        <header>
+           <p className="pageHeadre">
+             Welcome Back!
+           </p>
+        </header>
+        <main>
+          <form>
+          <input onChange = {nameChange} type="text" className='nameInput' placeholder='Name' id = "name" value = {name} />
+            <input onChange = {emailChange} type="text" className='emailInput' placeholder='Email' id = "email" value = {email} />
+           <div className='passwordInputDiv'>
+              <input type= {showPassword ? "text" : "password"} onChange = {passwordChange} className = 'passwordInput' placeholder='Password' value = {password} id = 'password' />
+              <img src= {visibilityIcon} alt="showPassword" className='showPassword' onClick = {() => checkShowPassword()} />
+            </div>
+          <Link to= '/forgotPassword' className='forgotPasswordLink' >forgot password</Link>
+            <div className='signUpBar'>
+              <p className="signUpText">
+                Register
+              </p>
+              <button className='signUpButton'>
+                <ArrowRightIcon fill = 'white' width = '34px' height = '34px'/>
+              </button>
+            </div>
+          </form>
+          <Link to='/login' className='registerLink'>Log In..if Already registered</Link>
+        </main>
+      </div>
     </div>
   )
 }
