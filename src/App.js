d6222e1cbab4b99bff892181be3fba408ebb38a4
Anjali Soni <anjalipatadiya@gmail.com>
2022-04-26 15:01:48 -0600

    Add private Route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Index from './components/Index'
+import Index from './components/Index';
 import Forgotpassword from './components/Forgotpassword';
 import Login from './components/Login';
-import Offer from './components/Offer';
 import Signin from './components/Signin';
 import Navbar from './components/Navbar';
 import User from './components/User';
+import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+
     <div className="App">
      <Router>
        <Routes>
          <Route path = '/' element={<Index/>}/>
-         <Route path = '/offers' element={<Offer/>}/>
-         <Route path = '/user' element={<User/>}/>
+         <Route path = '/user' element={<PrivateRoute />}>
+          <Route path = '/user' element={<User/>}/>
+         </Route>
+         <Route path='/user' element={<User />} />
          <Route path = '/signup' element={<Signin/>}/>
          <Route path = '/login' element={<Login/>}/>
          <Route path = '/forgotpassword' element={<Forgotpassword/>}/>
-
        </Routes>
        <Navbar/>
      </Router>
@@ -28,5 +30,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
