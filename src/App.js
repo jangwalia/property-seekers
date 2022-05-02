d3eb56a54b11d59cf6ffd3df565d5b123815d0c3
Anjali Soni <anjalipatadiya@gmail.com>
2022-05-02 13:43:42 -0600

    Chnage the forgotPassword link

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
          <Route path='/user' element={<User />} />
          <Route path = '/signup' element={<Signin/>}/>
          <Route path = '/login' element={<Login/>}/>
-         <Route path = '/forgotpassword' element={<Forgotpassword/>}/>
+         <Route path = '/forgotPassword' element={<Forgotpassword/>}/>
        </Routes>
        <Navbar/>
      </Router>
