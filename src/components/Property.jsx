e0d95dcebd8b9d6162013055ae18c2f662c80d55
umarkhan167 <77021740+umarkhan167@users.noreply.github.com>
2022-05-19 17:28:34 -0400

    Fix property layout for property section

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -8,57 +8,63 @@ import "./Property.css";
 export default function Property({ listingInfo, id, onDelete }) {
   // let regulerEx = '/\B(?=(\d{3})+(?!\d))/g'
   return (
+   
+    <div className="container mt-5">
     
-    
-     <div className="card-deck">
-      <div className="card shadow-lg p-8 mb-5 rounded ">
+     <div className="card mb-3">
+      <div className="row g-0">
         {/* <li className="categoryListing"> */}
-          <Link
-            to={`/categories/${listingInfo.type}/${id}`}
-          >
-            <img
-              src={listingInfo.imageUrls[0]}
-              alt="dataImg"
-              className="categoryListingImg card-img-top"
-            />
-            <div className="categoryListingDetails card-body">
-              <p className="categoryListingLocation">{listingInfo.location}</p>
-              <p className="categoryListingname">{listingInfo.name}</p>
-              <p className="categoryListingPrice">
-                $
-                {listingInfo.offer
-                  ? listingInfo.discountedPrice
-                  : listingInfo.regulerPrice}
-                {listingInfo.type === "rent" && " /month"}
-              </p>
-              <div className="categoryListingInfoDiv">
-                <img src={bedIcon} alt="" />
-                <p className="categroryListingInfoText">
-                  {listingInfo.bedrooms > 1
-                    ? `${listingInfo.bedrooms} Bedrooms`
-                    : `1 Bedroom`}
-                </p>
-                <img src={bathIcon} alt="" />
-                <p className="categroryListingInfoText">
-                  {listingInfo.bedrooms > 1
-                    ? `${listingInfo.bathrooms} Bathrooms`
-                    : `1 Bathrooms`}
-                </p>
+        <div className="col-md-4">
+                <Link
+              to={`/categories/${listingInfo.type}/${id}`}
+                >
+                <img
+                  src={listingInfo.imageUrls[0]}
+                  alt="dataImg"
+                  className="img-fluid"
+                />
+                </Link>
               </div>
-            </div>
-          </Link>
-          {onDelete && (
-            <DeleteIcon
-              className="removeIcon" 
-              width="5%" height="5%"
-              fill="tomato"
-              onClick={() => onDelete(listingInfo.id, listingInfo.name)}
-            />
-          )}
+              
+              <div className="col-md-8">
+                <div className="card-body">
+                  <p className="categoryListingLocation">{listingInfo.location}</p>
+                  <h5 className="categoryListingname">{listingInfo.name}</h5>
+                  <p className="categoryListingPrice">
+                    $
+                    {listingInfo.offer
+                      ? listingInfo.discountedPrice
+                      : listingInfo.regulerPrice}
+                    {listingInfo.type === "rent" && " /month"}
+                  </p>
+                  <div className="categoryListingInfoDiv">
+                    <img src={bedIcon} alt="" />
+                    <p className="categroryListingInfoText">
+                      {listingInfo.bedrooms > 1
+                        ? `${listingInfo.bedrooms} Bedrooms`
+                        : `1 Bedroom`}
+                    </p>
+                    <img src={bathIcon} alt="" />
+                    <p className="categroryListingInfoText">
+                      {listingInfo.bedrooms > 1
+                        ? `${listingInfo.bathrooms} Bathrooms`
+                        : `1 Bathrooms`}
+                    </p>
+                  </div>          
+                  {onDelete && (
+                  <DeleteIcon
+                    className="removeIcon" 
+                    width="5%" height="5%"
+                    fill="tomato"
+                    onClick={() => onDelete(listingInfo.id, listingInfo.name)}
+                  />
+                )}
+                </div>
+              </div>
         {/* </li> */}
       </div>
     </div>
-    
+    </div>
    
   );
 }
