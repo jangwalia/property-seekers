296808df5a085c8e3b55897ad31f111d50319e94
jang walia <jangiwalia83@gmail.com>
2022-05-20 07:27:32 -0700

    minor changes

---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgbkd7wLr1MHa2WBhFyeo3Pl2hVz8NP1s",
+  authDomain: "click-list-8ac18.firebaseapp.com",
+  projectId: "click-list-8ac18",
+  storageBucket: "click-list-8ac18.appspot.com",
+  messagingSenderId: "445414004157",
+  appId: "1:445414004157:web:44dd873cf2fea48910aed4"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFirestore()
